9f0bfff9dadbf3dc2323c4de22457d5f67c36419
Саша <@DESKTOP-KJAD6UE>
2022-02-07 20:08:20 +0200

    Add project files.

---
+++ ChangeSpaceOnTab/ChangeSpaceOnTab.cpp
@@ -0,0 +1,24 @@
+﻿// ChangeSpaceOnTab.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	string a;
+	getline(cin, a);
+	int size = a.size();//введення рядка
+	for (int i = 0; i < size; i++)
+	{
+		if (a[i] == ' ')//пошук пробілів в рядку
+		{
+			a[i] = '\t';//заміна пробілів на табуляцію
+		}
+	}
+	cout << a;//виведення зміненого рядка
+}
